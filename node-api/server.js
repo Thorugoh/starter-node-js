2ca3744e0d5bceaab33a57e9818d9c287ac26bd8
Victor Hugo <pavictorhugo@gmail.com>
2019-09-04 08:45:49 -0300

    criada api

---
+++ node-api/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Inciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+ 
+//Inciando o db
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    {useNewUrlParser: true}
+);
+
+requireDir('./src/models');
+
+//Rotas
+app.use('/api', require('./src/routes'))
+
+app.listen(3001);
